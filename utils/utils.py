25611dacc00cfc34fecfbf6aff24d224aa020454
CaseyCameron <mcasey.cameron@gmail.com>
2021-08-14 12:46:47 -0700

    Python styling refactor

--- utils/utils.py
+++ utils/utils.py
@@ -10,40 +10,44 @@ load_dotenv()
 env_path = Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
 
+#weatherbit env variables
 API_KEY = os.getenv("API_KEY")
 LAT = os.getenv("LAT")
 LON = os.getenv("LON")
 url = f"https://api.weatherbit.io/v2.0/current?lat={LAT}&lon={LON}&key={API_KEY}&include=minutely"
 
+# twilio env variables
 ACCOUNT_SID = os.getenv("ACCOUNT_SID")
 AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 FROM_ = os.getenv("FROM_")
 TO_ = os.getenv("TO_")
-
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-def fetchOutsideTemp():
+def fetch_outside_temp():
+  # grab the outside temp via weatherbit
   response = requests.get(url)
   data = json.loads(response.text)
   return data['data'][0]['temp']
 
-def convertTofahrenheit(temp_c):
+def convert_to_fahrenheit(temp_c):
+  # convert to Fahrenheit
   return (temp_c * (9 / 5)) + 32 
 
-def hasNotifiedToday(window_action):
+def has_notified_today(window_action):
   # test to see if file exists
   fname = Path(window_action)
-
   if fname.exists():
     return True
   return False
 
 def action_type(window_action):
+  # return either open or close to insert into text message
   if window_action == '.open_window':
     return 'open'
   return 'close'
 
 def notify(window_action):
+  # notify the user whether to open or close windows
   print(f'Take action: {window_action}')
 
   action = action_type(window_action)
@@ -57,6 +61,7 @@ def notify(window_action):
   f.write('')
   f.close()
     
-def cleanupFlag(flag):
+def cleanup_flag(flag):
+  # remove the flag if it exists
   if os.path.exists(flag):
     os.remove(flag)
